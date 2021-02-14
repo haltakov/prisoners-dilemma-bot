68ce8d573a02a02b05e4a91a63b0603e1264171a
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2021-02-14 22:03:07 +0100

    Change the bot output to game state

--- bot/prisoners_dilemma_bot.py
+++ bot/prisoners_dilemma_bot.py
@@ -1,29 +1,8 @@
 import time
 from bot.strategies import STRATEGIES
 
-BOT_REPLIES = dict(
-    rules="Hi, I'm the Prisoner's Dilemma Bot. We are going to play a game. Please reply with COOPERATE or DEFECT.",
-    invalid_move="Sorry, I could't understand which move you want to play. Please reply with either COOPERATE (or just C) or DEFECT (or just D)",
-    game_update="Move %d out of %d\n\nYour move: %s\nMy move: %s\n\nYou get %d points and I get %d points\n\nTotal points count:\nYou: %d\nMe: %d\n\n%s",
-    result_win="CONGRATILATIONS - You won!",
-    result_lose="LOSER - You lost the game!",
-    result_draw="The game is draw",
-)
 
 MAX_TIME_TO_REPLY = 2 * 86400  # 2 days
-
-
-def get_end_game_message(points):
-    if points[0] < points[1]:
-        return BOT_REPLIES["result_win"]
-    elif points[0] > points[1]:
-        return BOT_REPLIES["result_lose"]
-    else:
-        return BOT_REPLIES["result_draw"]
-
-
-def move_to_string(move):
-    return "C" if move else "D"
 
 
 class PrisonersDilemmaBot:
@@ -35,25 +14,18 @@ class PrisonersDilemmaBot:
         self.active_games = {}
         self.strategy = STRATEGIES.get(strategy, STRATEGIES[list(STRATEGIES.keys())[0]])
 
-    def play(self, user, text):
+    def play(self, user, opponent_move):
         # Get the active game
         game = self.active_games.get(user, None)
 
-        # Check if there is no active game for this` user
-        if not game:
-            return self.start_new_game(user)
+        # Check if there is no active game for this user or the user didn't play for a long time
+        if not game or time.time() - game["last_time"] > MAX_TIME_TO_REPLY:
+            self.active_games[user] = dict(
+                start_time=time.time(), last_time=time.time(), moves=[], points=(0, 0)
+            )
+            return self.active_games[user]
 
-        # Check if a new game should be started, because the user didn't play for a long time
-        if time.time() - game["last_time"] > MAX_TIME_TO_REPLY:
-            return self.start_new_game(user)
-
-        # Parse the opponent's move
-        try:
-            opponent_move = self.parse_move(text)
-        except ValueError:
-            return BOT_REPLIES["invalid_move"]
-
-        # Play the move
+        # Play both moves
         own_move = self.strategy(game["moves"])
         game["moves"].append((own_move, opponent_move))
 
@@ -65,57 +37,11 @@ class PrisonersDilemmaBot:
         )
         game["last_time"] = time.time()
 
-        # Check if this is the last game and prepare the end game message
-        moves_played = len(game["moves"])
-        end_game_message = ""
-        if moves_played == self.moves_to_play:
-            end_game_message = get_end_game_message(game["points"])
-
-        # Prepare the reply
-        reply = BOT_REPLIES["game_update"] % (
-            moves_played,
-            self.moves_to_play,
-            move_to_string(opponent_move),
-            move_to_string(own_move),
-            outcomes[1],
-            outcomes[0],
-            game["points"][1],
-            game["points"][0],
-            end_game_message,
-        )
-
-        # Delete the game if finished
-        if moves_played == self.moves_to_play:
+        # Check if the game is finished and delete
+        if len(game["moves"]) == self.moves_to_play:
             del self.active_games[user]
 
-        return reply
-
-    def start_new_game(self, user):
-        self.active_games[user] = dict(
-            start_time=time.time(), last_time=time.time(), moves=[], points=(0, 0)
-        )
-
-        return BOT_REPLIES["rules"]
-
-    def parse_move(self, text):
-        # Check for cooperate
-        if "COOPERATE" in text.upper():
-            return True
-
-        # Check for C
-        if text.upper().strip() == "C":
-            return True
-
-        # Check for defect
-        if "DEFECT" in text.upper():
-            return False
-
-        # Check for D
-        if text.upper().strip() == "D":
-            return False
-
-        # Raise exception if no valid move was found
-        raise ValueError("No valid move found")
+        return game
 
     def get_outcomes(self, own_move, opponent_move):
         if own_move and opponent_move:
--- tests/test_gameplay.py
+++ tests/test_gameplay.py
@@ -1,120 +1,87 @@
-from bot.prisoners_dilemma_bot import PrisonersDilemmaBot, BOT_REPLIES
+import time
+import pytest
+from unittest.mock import patch
+from bot.prisoners_dilemma_bot import PrisonersDilemmaBot
 
 
 def test_new_game():
     bot = PrisonersDilemmaBot()
 
-    assert bot.play("test_user", "@DilemmaBot let's play") == BOT_REPLIES["rules"]
-    assert bot.play("test_user_2", "@DilemmaBot let's play") == BOT_REPLIES["rules"]
+    expect_game_state(bot.play("test_user_1", True), [], (0, 0))
+    expect_game_state(bot.play("test_user_2", True), [], (0, 0))
 
 
-def test_invalid_move():
-    bot = PrisonersDilemmaBot()
-
-    bot.play("test_user", "@DilemmaBot let's play")
-    assert bot.play("test_user", "No valid move") == BOT_REPLIES["invalid_move"]
+def expect_game_state(game_state, expected_moves, expected_points):
+    assert game_state["moves"] == expected_moves
+    assert game_state["points"] == expected_points
 
 
 def test_short_game_draw():
     bot = PrisonersDilemmaBot(strategy="always_cooperate", moves_to_play=1)
 
     bot.play("test_user", "@DilemmaBot let's play")
-    assert bot.play("test_user", "C") == BOT_REPLIES["game_update"] % (
-        1,
-        1,
-        "C",
-        "C",
-        3,
-        3,
-        3,
-        3,
-        BOT_REPLIES["result_draw"],
-    )
+    expect_game_state(bot.play("test_user", True), [(True, True)], (3, 3))
 
 
 def test_short_game_win():
     bot = PrisonersDilemmaBot(strategy="always_cooperate", moves_to_play=1)
 
     bot.play("test_user", "@DilemmaBot let's play")
-    assert bot.play("test_user", "D") == BOT_REPLIES["game_update"] % (
-        1,
-        1,
-        "D",
-        "C",
-        5,
-        0,
-        5,
-        0,
-        BOT_REPLIES["result_win"],
-    )
+    expect_game_state(bot.play("test_user", False), [(True, False)], (0, 5))
 
 
 def test_short_game_lose():
     bot = PrisonersDilemmaBot(strategy="always_defect", moves_to_play=1)
 
     bot.play("test_user", "@DilemmaBot let's play")
-    assert bot.play("test_user", "C") == BOT_REPLIES["game_update"] % (
-        1,
-        1,
-        "C",
-        "D",
-        0,
-        5,
-        0,
-        5,
-        BOT_REPLIES["result_lose"],
-    )
+    expect_game_state(bot.play("test_user", True), [(False, True)], (5, 0))
 
 
 def test_long_game():
     bot = PrisonersDilemmaBot(moves_to_play=4)
 
     bot.play("test_user", "@DilemmaBot let's play")
-
-    assert bot.play("test_user", "C") == BOT_REPLIES["game_update"] % (
-        1,
-        4,
-        "C",
-        "C",
-        3,
-        3,
-        3,
-        3,
-        "",
+    expect_game_state(bot.play("test_user", True), [(True, True)], (3, 3))
+    expect_game_state(
+        bot.play("test_user", False), [(True, True), (True, False)], (3, 8)
+    )
+    expect_game_state(
+        bot.play("test_user", False),
+        [(True, True), (True, False), (False, False)],
+        (4, 9),
+    )
+    expect_game_state(
+        bot.play("test_user", True),
+        [(True, True), (True, False), (False, False), (False, True)],
+        (9, 9),
     )
 
-    assert bot.play("test_user", "D") == BOT_REPLIES["game_update"] % (
-        2,
-        4,
-        "D",
-        "C",
-        5,
-        0,
-        8,
-        3,
-        "",
+
+def test_parallel_games():
+    bot = PrisonersDilemmaBot(moves_to_play=2)
+
+    bot.play("test_user_1", "@DilemmaBot let's play")
+    expect_game_state(bot.play("test_user_1", True), [(True, True)], (3, 3))
+
+    bot.play("test_user_2", "@DilemmaBot let's play")
+    expect_game_state(bot.play("test_user_2", False), [(True, False)], (0, 5))
+
+    expect_game_state(
+        bot.play("test_user_1", False), [(True, True), (True, False)], (3, 8)
     )
 
-    assert bot.play("test_user", "D") == BOT_REPLIES["game_update"] % (
-        3,
-        4,
-        "D",
-        "D",
-        1,
-        1,
-        9,
-        4,
-        "",
+    expect_game_state(
+        bot.play("test_user_2", False), [(True, False), (False, False)], (1, 6)
     )
 
-    assert bot.play("test_user", "C") == BOT_REPLIES["game_update"] % (
-        4,
-        4,
-        "C",
-        "D",
-        0,
-        5,
-        9,
-        9,
-        BOT_REPLIES["result_draw"],
-    )
+
+def test_game_timeout():
+    bot = PrisonersDilemmaBot(moves_to_play=2)
+
+    bot.play("test_user_1", "@DilemmaBot let's play")
+    expect_game_state(bot.play("test_user_1", True), [(True, True)], (3, 3))
+
+    bot.active_games["test_user_1"]["start_time"] -= 200000
+    bot.active_games["test_user_1"]["last_time"] -= 200000
+
+    expect_game_state(bot.play("test_user_1", True), [], (0, 0))
